92d34e697197a2a0cdd2f922871939f541d85706
Animikh Aich <animikhaich@gmail.com>
2018-07-27 21:13:43 +0530

    Organized and Integrated the project

--- TelegramAPI.py
+++ TelegramAPI.py
@@ -5,40 +5,72 @@ context = None
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
+
+help_text = """
+Welcome to your DoorBot. I automatically decide who goes into your house for you, with your permission of course :)
+
+Let me introduce you to the commands:
+
+/help: That's me! At your service! 
+/photo: Send the most recent picture of the guest in the Database
+/all_photo: Send all the pictures of the guest in the Database
+"""
+
+
 def start(bot, update):
     print('Received /start command')
     update.message.reply_text('Hi!')
 
 
 def help(bot, update):
-    print('Received /help command')
-    update.message.reply_text('Help!')
+    update.message.reply_text(help_text)
 
 
 def echo(bot, update):
-    print('Received an update')
-    update.message.reply_text(update.message.text.upper())
+    print(update.message.text)
+    if update.message.text.lower() == "open door" or update.message.text.lower() == "open it":
+        update.message.reply_text("Door Opened")
+    else:
+        update.message.reply_text("Door stays closed")
+
+
+def send_most_recent_pic(bot, update):
+    try:
+        update.message.reply_text("Sending most recent picture...")
+        bot.send_photo(chat_id=update.message.chat_id, photo=open('./DataBase/test_pic_4.jpg', 'rb'))
+    except Exception as e:
+        print("Could not read file from Database. Picture sending failed")
+        update.message.reply_text("Unable to send picture due to the error: " + str(e))
+
+
+def send_all_pics(bot, update):
+    try:
+        update.message.reply_text("Sending all pictures...")
+        for pic in range(5):
+            bot.send_photo(chat_id=update.message.chat_id, photo=open('./DataBase/test_pic_' + str(pic) + '.jpg', 'rb'))
+    except Exception as e:
+        print("Could not read file from Database. Picture sending failed")
+        update.message.reply_text("Unable to send picture due to the error: " + str(e))
 
 
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater('API_KEY')
+    updater = Updater('629898347:AAG4beXS5T7KwlLfbTdtnSgAzbIUec55-ik')
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("photo", send_most_recent_pic))
+    dp.add_handler(CommandHandler("all_photo", send_all_pics))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     # Start the Bot
     updater.start_polling()
-    # Block until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+
     updater.idle()
 
 
--- face_recg.py
+++ face_recg.py
@@ -1,50 +1,52 @@
 import face_recognition
 import cv2
+import way2sms
 
-# Get a reference to webcam #0
-video_capture = cv2.VideoCapture(0)
-
-# Load a sample picture and learn how to recognize it.
-animikh_image = face_recognition.load_image_file("ani_2.jpg")
-animikh_face_encoding = face_recognition.face_encodings(animikh_image)[0]
-
-akhilesh_image = face_recognition.load_image_file("akhil_1.jpg")
-akhilesh_face_encoding = face_recognition.face_encodings(akhilesh_image)[0]
+q = way2sms.Sms('9611933016', '2671996qw')
 
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    animikh_face_encoding,
-    akhilesh_face_encoding
-]
-known_face_names = [
-    "Animikh",
-    "Akhilesh"
-]
+def capture_picture():
+    # Get a reference to webcam #0
+    video_capture = cv2.VideoCapture(0)
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+    # Load a sample picture and learn how to recognize it.
+    animikh_image = face_recognition.load_image_file("./Assets/ani_2.jpg")
+    animikh_face_encoding = face_recognition.face_encodings(animikh_image)[0]
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+    akhilesh_image = face_recognition.load_image_file("./Assets/akhil_1.jpg")
+    akhilesh_face_encoding = face_recognition.face_encodings(akhilesh_image)[0]
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Create arrays of known face encodings and their names
+    known_face_encodings = [
+        animikh_face_encoding,
+        akhilesh_face_encoding
+    ]
+    known_face_names = [
+        "Animikh",
+        "Akhilesh"
+    ]
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
+    for pic in range(5):
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+        cv2.imwrite("./DataBase/test_pic_" + str(pic) + ".jpg", frame)
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
@@ -56,33 +58,40 @@ while True:
                 name = known_face_names[first_match_index]
 
             face_names.append(name)
-            print(face_names)
 
-    process_this_frame = not process_this_frame
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.imwrite("./DataBase/test_pic_" + str(pic) + ".jpg", frame)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+    print(face_names)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    boolean_list = [True for name in face_names if name in known_face_names]
+    boolean = any(boolean_list)
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    if boolean:
+        q.send('9611933016', 'The following guests have been allowed inside: ' + ' '.join(set(face_names)))
+    else:
+        q.send('9611933016', 'Unknown person wants to enter. Please Enquire telegram for further details')
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+    q.logout()
+
+
+if __name__ == '__main__':
+    capture_picture()
